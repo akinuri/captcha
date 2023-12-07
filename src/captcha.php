69738a709e68891fd4f2e42c83c75e2200741fee
Noreh AD <akinoreh@gmail.com>
2023-12-07 13:08:45 +0300

    buildCaptcha(): pad args

--- src/captcha.php
+++ src/captcha.php
@@ -4,14 +4,17 @@ require_once __DIR__ . "/random.php";
 require_once __DIR__ . "/image.php";
 
 function buildCaptcha(
-    string|int $string,
+    string|int $stringOrLength,
     int $fontSize = 16,
+    int $padX = 5,
+    int $padY = 5,
     int $obscurityLines = 2,
 ) {
+    $string = $stringOrLength;
     if (is_int($string)) {
-        $string = randomString($string);
+        $string = randomString($stringOrLength);
     }
-    $image = imageFromString($string, $fontSize, 5, 5);
+    $image = imageFromString($string, $fontSize, $padX, $padY);
     for ($i = 0; $i < $obscurityLines; $i++) {
         drawCrossLine($image);
     }
